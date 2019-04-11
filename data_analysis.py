93186ca67f57ce15da1ba1ef095c538e27be80a7
LelandYan <2721599586@qq.com>
2019-04-12 00:25:39 +0800

    mul_info

--- data_analysis.py
+++ data_analysis.py
@@ -1,3 +1,65 @@
 # _*_ coding: utf-8 _*_
-__author__ = 'LelandYan'
+__author__ = 'Yxp'
 __date__ = '2019/4/11 17:40'
+import numpy as np
+import pandas as pd
+from scipy.stats import pearsonr
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, RandomizedLasso
+from sklearn.feature_selection import RFE, f_regression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn import metrics as mr
+
+
+class cal_corr:
+    def __init__(self, name="result.xlsx"):
+        self.name = name
+        self.pearsonr_rate = []
+        self.mutual_info_rate = []
+
+    def read_data(self):
+        # 读取数据
+        self.data = pd.read_excel("result.xlsx")
+        # 获得C收得率
+        self.C_label = self.data.iloc[:, -2]
+        # 获得Mn收得率
+        self.Mn_label = self.data.iloc[:, -1]
+        # 获取属性
+        self.data = self.data.iloc[:, :-2]
+
+    def pearsonr_cal(self):
+        for i in np.arange(self.data.shape[1]):
+            self.pearsonr_rate.append(pearsonr(self.C_label, self.data.iloc[:, i])[1])
+        self.pearsonr_rate = np.array(self.pearsonr_rate)
+
+    def corr_cal(self):
+        self.f, pval = f_regression(self.C_label, self.data, center=True)
+
+    def mutual_info(self,C_label=True):
+        if C_label:
+            label = self.C_label
+        else:
+            label = self.Mn_label
+        for i in np.arange(self.data.shape[1]):
+            self.mutual_info_rate.append(mr.mutual_info_score(label, self.data.iloc[:, i]))
+        self.mutual_info_rate = np.array(self.mutual_info_rate)
+
+    def run(self, pearsonr_cal=False, corr_cal=False, mutual_info=True):
+        self.read_data()
+        if pearsonr_cal:
+            self.pearsonr_cal()
+            return self.pearsonr_rate
+        if corr_cal:
+            self.corr_cal()
+            return self.f
+        if mutual_info:
+            self.mutual_info()
+            return self.mutual_info_rate
+
+
+if __name__ == '__main__':
+    pass
+    item = cal_corr()
+    print(item.run())
--- gen_raw_data.py
+++ gen_raw_data.py
@@ -5,44 +5,61 @@ __date__ = '2019/4/11 13:27'
 import numpy as np
 import pandas as pd
 
-# 获取整个数据集
-data = pd.read_excel("D题附件1.xlsx")
-label = pd.read_excel("D题附件2.xlsx")
-label = label.fillna(0)
-data = data[["转炉终点C", '转炉终点Mn', '钢水净重', '连铸正样C', '连铸正样Mn',
-             "钒铁(FeV50-A)", "钒铁(FeV50-B)", "硅铝合金FeAl30Si25", "硅铝锰合金球",
-             "硅锰面（硅锰渣）", "硅铁(合格块)", "硅铁FeSi75-B", "石油焦增碳剂",
-             "锰硅合金FeMn64Si27(合格块)", "锰硅合金FeMn68Si18(合格块)", "碳化硅(55%)", "硅钙碳脱氧剂","转炉终点温度"]]
 
-# 251行之后转炉终点Mn为空
-data = data[:251]
-data_com = data.iloc[:, 5:]
+class gen_data:
+    def __init__(self, data="D题附件1.xlsx", label="D题附件2.xlsx"):
+        self.data_name = data
+        self.label_name = label
 
-# C的历史回收率
-data_c = data_com.iloc[:, [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
-C = label.iloc[:, 1]
-components = (C != 0)
-components = np.array(label.iloc[:, 1][components])
-div_num = data_c * components
-sum_data = div_num.sum(axis=1)
-data["C收得率"] = (data['连铸正样C'] - data["转炉终点C"]) * data['钢水净重'] / sum_data
-# # Mn的历史回收率
-data_Mn = data_com.iloc[:, [3, 4, 8, 9]]
-Mn = label.iloc[:, 2]
-components = (Mn != 0)
-components = np.array(label.iloc[:, 2][components])
-div_num = data_Mn * components
-sum_data = div_num.sum(axis=1)
-data["Mn收得率"] = (data['连铸正样Mn'] - data["转炉终点Mn"]) * data['钢水净重'] / sum_data
+    def read_data(self):
+        # 获取整个数据集
+        self.data = pd.read_excel("D题附件1.xlsx")
+        self.label = pd.read_excel("D题附件2.xlsx")
+        self.label = self.label.fillna(0)
+        self.data = self.data[["转炉终点C", '转炉终点Mn', '钢水净重', '连铸正样C', '连铸正样Mn',
+                               "钒铁(FeV50-A)", "钒铁(FeV50-B)", "硅铝合金FeAl30Si25", "硅铝锰合金球",
+                               "硅锰面（硅锰渣）", "硅铁(合格块)", "硅铁FeSi75-B", "石油焦增碳剂",
+                               "锰硅合金FeMn64Si27(合格块)", "锰硅合金FeMn68Si18(合格块)", "碳化硅(55%)", "硅钙碳脱氧剂", "转炉终点温度"]]
+        # 251行之后转炉终点Mn为空
+        self.data = self.data[:251]
+        self.data_com = self.data.iloc[:, 5:]
 
-# 对结果进行处理 去除inf和null值
-data.fillna(np.inf, inplace=True)
-data = data[~data["Mn收得率"].isin(['inf'])]
-data = data[~data["C收得率"].isin(['inf'])]
+    def cal_C(self):
+        # C的历史回收率
+        data_c = self.data_com.iloc[:, [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
+        C = self.label.iloc[:, 1]
+        components = (C != 0)
+        components = np.array(self.label.iloc[:, 1][components])
+        div_num = data_c * components
+        sum_data = div_num.sum(axis=1)
+        self.data["C收得率"] = (self.data['连铸正样C'] - self.data["转炉终点C"]) * self.data['钢水净重'] / sum_data
 
-# 将收得率大于1的进行剔除
-data = data[data["C收得率"] < 1]
-# 将转炉终点温度为0的剔除
-data = data[data["转炉终点温度"] != 0]
-# 储存结果文件
-data.to_excel("result.xlsx", index=False)
+    def cal_Mn(self):
+        # # Mn的历史回收率
+        data_Mn = self.data_com.iloc[:, [3, 4, 8, 9]]
+        Mn = self.label.iloc[:, 2]
+        components = (Mn != 0)
+        components = np.array(self.label.iloc[:, 2][components])
+        div_num = data_Mn * components
+        sum_data = div_num.sum(axis=1)
+        self.data["Mn收得率"] = (self.data['连铸正样Mn'] - self.data["转炉终点Mn"]) * self.data['钢水净重'] / sum_data
+
+    def run(self):
+        self.read_data()
+        self.cal_C()
+        self.cal_Mn()
+        # 对结果进行处理 去除inf和null值
+        self.data.fillna(np.inf, inplace=True)
+        self.data = self.data[~self.data["Mn收得率"].isin(['inf'])]
+        self.data = self.data[~self.data["C收得率"].isin(['inf'])]
+
+        # 将收得率大于1的进行剔除
+        self.data = self.data[self.data["C收得率"] < 1]
+        # 将转炉终点温度为0的剔除
+        self.data = self.data[self.data["转炉终点温度"] != 0]
+        # 储存结果文件
+        self.data.to_excel("result.xlsx", index=False)
+
+if __name__ == '__main__':
+    item = gen_data()
+    item.run()
